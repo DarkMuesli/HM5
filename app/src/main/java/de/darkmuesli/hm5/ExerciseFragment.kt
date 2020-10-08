3d7ab451b86b640a160e99d516b4ba55741089c3
Matthias Groß <mg-rs@gmx.de>
2020-10-08 02:07:10 +0200

    First working randomization. Dirty hack, but works.

--- app/src/main/java/de/darkmuesli/hm5/ExerciseFragment.kt
+++ app/src/main/java/de/darkmuesli/hm5/ExerciseFragment.kt
@@ -5,11 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_exercise.*
+import kotlin.random.Random
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_EXERCISES = "exercises"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,14 +19,12 @@ private const val ARG_PARAM2 = "param2"
  */
 class ExerciseFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var exercises: Array<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            exercises = it.getStringArray(ARG_EXERCISES)
         }
     }
 
@@ -38,22 +37,24 @@ class ExerciseFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_exercise, container, false)
     }
 
+    fun randomizeExercise() {
+        exerciseTextView.text = exercises?.let { it[Random.nextInt() % it.size] }
+    }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
+         * @param exercises Parameter 1.
          * @return A new instance of fragment ExerciseFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(exercises: Array<String>) =
             ExerciseFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putStringArray(ARG_EXERCISES, exercises)
                 }
             }
     }
--- app/src/main/java/de/darkmuesli/hm5/MainActivity.kt
+++ app/src/main/java/de/darkmuesli/hm5/MainActivity.kt
@@ -2,10 +2,26 @@ package de.darkmuesli.hm5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.abs
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onButtonPress(view: View) {
+        val tonalityText = findViewById<TextView>(R.id.tonality_text)
+        val exerciseText = findViewById<TextView>(R.id.exerciseTextView)
+
+        val tonalities = resources.getStringArray(R.array.tonalities)
+        val exercises = resources.getStringArray(R.array.exercises)
+
+        tonalityText.text = tonalities[abs(Random.nextInt() % tonalities.size)]
+        exerciseText.text = exercises[abs(Random.nextInt() % exercises.size)]
     }
 }
