4222c43d9d4894963c71d7ac82a09dda3ad873aa
Matthias Groß <mg-rs@gmx.de>
2020-10-13 16:12:08 +0200

    Cleanup, no longer using views directly.

--- app/src/main/java/de/darkmuesli/hm5/MainActivity.kt
+++ app/src/main/java/de/darkmuesli/hm5/MainActivity.kt
@@ -3,14 +3,14 @@ package de.darkmuesli.hm5
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        exerciseCard.setOnClickListener {
+        findViewById<CardView>(R.id.exerciseCard).setOnClickListener {
             startActivity(Intent(this, ExerciseActivity::class.java))
         }
     }
--- app/src/main/java/de/darkmuesli/hm5/ui/exercise/TonalityFragment.kt
+++ app/src/main/java/de/darkmuesli/hm5/ui/exercise/TonalityFragment.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import de.darkmuesli.hm5.R
-import kotlinx.android.synthetic.main.tonality_fragment.*
 
 class TonalityFragment : Fragment() {
 
@@ -23,7 +23,7 @@ class TonalityFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         exerciseViewModel.currentTonality.observe(viewLifecycleOwner, { tonality ->
-            tonalityTextView.text = tonality
+            view.findViewById<TextView>(R.id.tonalityTextView).text = tonality
         })
     }
 }
