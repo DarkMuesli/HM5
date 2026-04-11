195b408c8a1b38d2d7703e495d902bc46af26204
Matthias Groß <mg-rs@gmx.de>
2026-04-11 13:09:17 +0200

    Add system bars and cutouts paddings

--- app/src/main/java/de/darkmuesli/hm5/MainActivity.kt
+++ app/src/main/java/de/darkmuesli/hm5/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import de.darkmuesli.hm5.databinding.ActivityMainBinding
 import de.darkmuesli.hm5.ui.exercise.ExerciseActivity
 
@@ -13,7 +15,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val displayCutout = insets.getInsets(WindowInsetsCompat.Type.displayCutout())
+            view.setPadding(
+                maxOf(systemBars.left, displayCutout.left),
+                maxOf(systemBars.top, displayCutout.top),
+                maxOf(systemBars.right, displayCutout.right),
+                maxOf(systemBars.bottom, displayCutout.bottom)
+            )
+            insets
+        }
 
         binding.exerciseCard.setOnClickListener {
             startActivity(Intent(this, ExerciseActivity::class.java))
--- app/src/main/java/de/darkmuesli/hm5/ui/exercise/ExerciseActivity.kt
+++ app/src/main/java/de/darkmuesli/hm5/ui/exercise/ExerciseActivity.kt
@@ -3,13 +3,29 @@ package de.darkmuesli.hm5.ui.exercise
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import de.darkmuesli.hm5.R
 
 class ExerciseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.exercise_activity)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragmentContainer)) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val displayCutout = insets.getInsets(WindowInsetsCompat.Type.displayCutout())
+            view.setPadding(
+                maxOf(systemBars.left, displayCutout.left),
+                maxOf(systemBars.top, displayCutout.top),
+                maxOf(systemBars.right, displayCutout.right),
+                maxOf(systemBars.bottom, displayCutout.bottom)
+            )
+            insets
+        }
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
